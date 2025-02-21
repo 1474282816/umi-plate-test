eeba8313b506e5ea37b59a16c2da4c96c84e2cfd
renrunqi <38024067+1474282816@users.noreply.github.com>
2025-02-21 04:38:55 +0000

    修复文件缺失

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
+    {
+      name: 'plate',
+      path: '/plate',
+      component: '../app/editor/page.tsx',
+    },
   ],
   npmClient: 'npm',
 });
